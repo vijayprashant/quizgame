343a15e4d027fa8a815238027f0a5b872796cd39
vijayprashant <141649303+vijayprashant@users.noreply.github.com>
2023-08-07 19:58:18 +0530

    Add files via upload

---
+++ projectquizout.c
@@ -0,0 +1,46 @@
+Welcome to the Game
+>Press 7 to start the game>Press 0 to quit the game7
+The game has started
+
+1)Which one is the first search engine in internet?
+
+1)Google
+2)Archive
+3)Wais
+4)Altavista
+Enter Your Answer:2
+Correct AnswerYou have scored 5 point
+2)Which one is the first web browser invented in 1990?
+
+1)Internet Explorer
+2)Mosaic
+3)Mozilla
+4)Nexus
+Enter Your Answer:3
+Wrong Answer
+You have scored 0 point
+3)First computer virus is known as?
+
+1)Rabbit
+2)Creeper Virus
+3)Elk Cloner
+4)SCA Virus
+Enter Your Answer:2
+Correct AnswerYou have scored 5 point
+4)Firewall in computer is used for?
+
+1)Security
+2)Data Transmission
+3)Monitering
+4)Autentication
+Enter Your Answer:1
+Correct AnswerYou have scored 5 point
+5)Which of the following is not a database management software?
+
+1)Mysql
+2)Oracle
+3)cobal
+4)Sybase
+Enter Your Answer:4
+Wrong Answer
+You have scored 0 point
